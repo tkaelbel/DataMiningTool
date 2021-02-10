964e963b9759bfc78674eda063fc0d355b5e62f7
Tim K <tkaelbel@web.de>
2021-02-10 14:14:41 +0100

    Updated algorithmanager
    Added assertions and also the call of the algorithm factory

--- src/main/java/com/tok/data/mining/algorithms/AlgorithmManager.java
+++ src/main/java/com/tok/data/mining/algorithms/AlgorithmManager.java
@@ -1,11 +1,15 @@
 package com.tok.data.mining.algorithms;
 
-import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
+
+import com.tok.data.mining.payload.request.AlgorithmRequest;
+import com.tok.data.mining.payload.response.AlgorithmResponse;
 
 @Component
 public class AlgorithmManager {
@@ -20,6 +24,20 @@ public class AlgorithmManager {
 			names.add(name.toString());
 		}
 		return names;
+	}
+	
+	public AlgorithmResponse executeAlgorithm(AlgorithmRequest request) {
+		Assert.notNull(request, "request object is null");
+		Assert.notNull(request.getName(), "algorithName is null");
+		Assert.notNull(request.getProperties(), "properties is null");
+		Assert.notNull(request.getDatabaseName(), "databaseName is null");
+		Assert.notNull(request.getCollectionName(), "collectionName is null");
+		
+		IAlgorithm algorithm = algorithmFactory.getAlgorithm(request.getEnumName());
+		if(algorithm != null) algorithm.execute(request);
+		
+		
+		return null;
 	}
 	
 }
