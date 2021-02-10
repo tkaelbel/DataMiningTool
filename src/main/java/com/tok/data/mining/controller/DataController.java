efdcdaf5220492c147bf8029d2f4a99c005f26d3
Tim K <tkaelbel@web.de>
2021-02-10 14:16:01 +0100

    Added executeAlgorihtm POST service

--- src/main/java/com/tok/data/mining/controller/DataController.java
+++ src/main/java/com/tok/data/mining/controller/DataController.java
@@ -3,16 +3,22 @@ package com.tok.data.mining.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tok.data.mining.algorithms.AlgorithmManager;
 import com.tok.data.mining.mongodb.MongoDbCentral;
+import com.tok.data.mining.payload.request.AlgorithmRequest;
 import com.tok.data.mining.payload.request.CollectionDataRequest;
 import com.tok.data.mining.payload.response.AlgorithmNamesResponse;
+import com.tok.data.mining.payload.response.AlgorithmResponse;
 import com.tok.data.mining.payload.response.CollectionDataResponse;
 import com.tok.data.mining.payload.response.DatabaseDataResponse;
 
@@ -43,4 +49,10 @@ public class DataController {
 	public ResponseEntity<AlgorithmNamesResponse> algorithmNames() {
 		return ResponseEntity.ok(new AlgorithmNamesResponse(algorithmManager.getRegisteredAlgorithms()));
 	}
+	
+	@PostMapping("/executeAlgorithm")
+	public ResponseEntity<AlgorithmResponse> executeAlgorithm(@Validated @RequestBody AlgorithmRequest request) {
+		return ResponseEntity.ok(algorithmManager.executeAlgorithm(request));
+	}
+	
 }
