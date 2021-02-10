5caa049198c4f21ad71403f7d127bfd9dab83183
Tim K <tkaelbel@web.de>
2021-02-10 14:15:25 +0100

    Added algorithm models

---
+++ src/main/java/com/tok/data/mining/algorithms/apriori/AprioriReturnModel.java
@@ -0,0 +1,8 @@
+package com.tok.data.mining.algorithms.apriori;
+
+import com.tok.data.mining.algorithms.IAlgorithmReturnModel;
+
+public class AprioriReturnModel implements IAlgorithmReturnModel {
+
+	
+}
---
+++ src/main/java/com/tok/data/mining/algorithms/models/AlgorithmModel.java
@@ -0,0 +1,5 @@
+package com.tok.data.mining.algorithms.models;
+
+public class AlgorithmModel {
+
+}
