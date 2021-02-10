b0808a1a82481245a78575a5e1bc94ab68c1e35f
Tim K <tkaelbel@web.de>
2021-02-10 14:15:39 +0100

    Adjusted IAlgorithm interface

--- src/main/java/com/tok/data/mining/algorithms/IAlgorithm.java
+++ src/main/java/com/tok/data/mining/algorithms/IAlgorithm.java
@@ -1,10 +1,15 @@
 package com.tok.data.mining.algorithms;
 
+import com.tok.data.mining.algorithms.models.AlgorithmModel;
+import com.tok.data.mining.payload.request.AlgorithmRequest;
+
 public interface IAlgorithm {
 	
-	public IAlgorithmReturnModel execute();
+	public IAlgorithmReturnModel execute(AlgorithmRequest data);
 
 	public AlgorithmName getName();
+	
+	public AlgorithmModel getModel(Object inputProperties);
 	
 }
 
